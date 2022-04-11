2b622d2ddc4d0cb8e76ea215e366bfb1191e175c
Malgorzata <96203042+MProtokowicz@users.noreply.github.com>
2022-04-11 16:55:37 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import
+
+
+function App() {
+  return (
+    <div className="app">
+    <main>
+      <div className="header"> 
+      <div className="text1">Choose your book</div>
+      <br></br>
+                  <div className="search-box"> 
+                  <input 
+                  type="text"
+                  className="search-bar"
+                  placeholder="Search..."
+                  />
+                </div>
+        </div> 
+    </main>
+    </div>
+    
+  );
+}
+
+export default App;
+
+
+
